0328378bca9fd3c3725d4f33bf2748c6ccb74c18
FlorenciaCasal <florenciacasal88.fc@gmail.com>
2025-06-27 16:41:14 -0300

    prueba produccion

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,6 @@ module.exports = {
       screens: {
         'xs': '480px',  // Breakpoint personalizado
       },
-      // colors: {
-      //   'circulos': '#C1FD35',
-      //    'circuloshover': '#A0E529',
-      // },
-      // animation: {
-      //   fadeIn: 'fadeIn 0.3s ease-out forwards',
-      //   fadeInUp: 'fadeInUp 0.3s ease-out',
-      // },
-      // keyframes: {
-      //   fadeIn: {
-      //     '0%': { opacity: '0', transform: 'scale(0.95)' },
-      //     '100%': { opacity: '1', transform: 'scale(1)' },
-      //   },
-      //   fadeInUp: {
-      //     '0%': { opacity: '0', transform: 'translateY(10px)' },
-      //     '100%': { opacity: '1', transform: 'translateY(0)' },
-      //   },
-      // },
     },
   },
   plugins: [],
