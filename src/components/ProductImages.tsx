5809566b52a71fc8ca8a5cc2cc64fdffc9b077f9
FlorenciaCasal <florenciacasal88.fc@gmail.com>
2025-07-04 16:16:57 -0300

    imagenes aun mejor

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -5,7 +5,10 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useSwipeable } from 'react-swipeable';
 import Image from 'next/image';
 import Lightbox from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 type Props = {
     images: ProductImage[];
@@ -29,26 +32,20 @@ export default function ProductImages({ images, productName }: Props) {
     return (
         <>
             <div className="flex flex-col md:flex-row gap-4">
-                {/* Miniaturas */}
+                {/* Miniaturas laterales en desktop */}
                 <div className="hidden md:flex md:flex-col gap-2 md:w-1/4 justify-around">
                     {images.map((img, idx) => (
                         <div
                             key={img.id}
                             onClick={() => setSelectedIndex(idx)}
-                            className={`cursor-pointer rounded border-2 transition-all relative w-full h-20 ${selectedIndex === idx ? 'border-gray-800' : 'border-transparent'
-                                } hover:opacity-80`}
+                            className={`cursor-pointer rounded border-2 transition-all relative w-full h-20 ${selectedIndex === idx ? 'border-gray-800' : 'border-transparent'} hover:opacity-80`}
                         >
-                            <Image
-                                src={img.url}
-                                alt={productName}
-                                fill
-                                className="object-cover rounded"
-                            />
+                            <Image src={img.url} alt={productName} fill className="object-cover rounded" />
                         </div>
                     ))}
                 </div>
 
-                {/* Imagen Principal */}
+                {/* Imagen principal */}
                 <div className="md:w-3/4 w-full" {...handlers}>
                     <div
                         className="relative w-full h-[300px] md:h-[500px] rounded cursor-zoom-in"
@@ -74,8 +71,7 @@ export default function ProductImages({ images, productName }: Props) {
                             <button
                                 key={idx}
                                 onClick={() => setSelectedIndex(idx)}
-                                className={`w-3 h-3 rounded-full ${selectedIndex === idx ? 'bg-blue-500' : 'bg-gray-400'
-                                    }`}
+                                className={`w-3 h-3 rounded-full ${selectedIndex === idx ? 'bg-blue-500' : 'bg-gray-400'}`}
                             />
                         ))}
                     </div>
@@ -86,30 +82,28 @@ export default function ProductImages({ images, productName }: Props) {
                             <div
                                 key={img.id}
                                 onClick={() => setSelectedIndex(idx)}
-                                className={`cursor-pointer rounded border-2 flex-shrink-0 w-20 h-20 relative ${selectedIndex === idx ? 'border-blue-500' : 'border-transparent'
-                                    }`}
+                                className={`cursor-pointer rounded border-2 flex-shrink-0 w-20 h-20 relative ${selectedIndex === idx ? 'border-blue-500' : 'border-transparent'}`}
                             >
-                                <Image
-                                    src={img.url}
-                                    alt={productName}
-                                    fill
-                                    className="object-cover rounded"
-                                />
+                                <Image src={img.url} alt={productName} fill className="object-cover rounded" />
                             </div>
                         ))}
                     </div>
                 </div>
             </div>
 
-            {/* Lightbox */}
+            {/* Lightbox fullscreen con zoom y thumbnails */}
             <Lightbox
                 open={open}
                 close={() => setOpen(false)}
                 index={selectedIndex}
                 slides={images.map((img) => ({ src: img.url }))}
+                plugins={[Zoom, Thumbnails]}
+                styles={{ container: { backgroundColor: 'rgba(0, 0, 0, 0.95)' } }}
+                animation={{ fade: 300 }}
             />
         </>
     );
 }
+
 
 
